51648e797cdc315776bfab5a5f392793d91e49a0
Shreyanshu Shankar <shreyanshu.shankar@getmyuni.com>
2019-11-29 19:17:03 +0530

    getteams data

---
+++ apis/teams/getAllTeams.py
@@ -0,0 +1,36 @@
+import sys
+from flask import Flask, request, Blueprint
+
+sys.path.append('../../')
+from lib.tokens.AuthTokens import jwtTokenVerify
+from lib.mongo.TeamsOperation import Teams
+
+
+
+team = Blueprint('getTeams', __name__)
+
+
+@team.route('/v1/user/getTeams', methods = ['POST'])
+def test_route():
+    request_json = request.get_json()
+    key = request.headers.get('Token')
+    val = request.headers.get('secret_key')
+    if key and val :
+        token_satus, val = jwtTokenVerify(val, key)
+        if token_satus:
+            team_ob = Teams()
+            response = team_ob.getAllTeams()
+            return response,200
+        else:
+            response = {
+                "message":"Failed token Validation"
+            }
+            return response,402 
+
+    else:
+        response ={
+            "message" : "key validation error"
+        }
+        return response, 404
+
+
